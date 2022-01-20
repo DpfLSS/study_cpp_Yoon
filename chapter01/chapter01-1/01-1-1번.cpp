a71b906a0c06af2eeb4eb7a9c5118d4147623169
DpfLSS <97493637+DpfLSS@users.noreply.github.com>
2022-01-20 21:14:09 +0900

    220119

--- chapter01/chapter01-1/01-1-1번.cpp
+++ chapter01/chapter01-1/01-1-1번.cpp
@@ -6,13 +6,13 @@ int main(void)
 	int result = 0;
 	for (int i = 1; i < 6; i++)
 	{
-		std::cout << i << "¹øÂ° Á¤¼ö ÀÔ·Â:";
+		std::cout << i << "번째 정수 입력:";
 		std::cin >> n;
 		result = result + n;
 
 	}
 
-	std::cout << "ÇÕ°è:" << result << std::endl;
+	std::cout << "합계:" << result << std::endl;
 
 	return 0;
 
