bf07832d99e329d290fdb3b2d85548ed226dc6a6
예린 정 <jung08140070@gmail.com>
2022-02-07 22:21:21 +0900

    220207

--- chapter02-1/chapter02/malloc.cpp
+++ chapter02-1/chapter02/malloc.cpp
@@ -6,7 +6,7 @@ int main(void)
 	int* pi;
 	int i, sum = 0;
 
-	pi = (int*)malloc(5* sizeof(int));
+	pi = (int*)malloc(sizeof(int) * 5); // int*형의 원소를 5개 가지는 1차원 배열 생성
 	if (pi == NULL)
 	{
 		std::cout << "메모리가 부족합니다." << std::endl;
@@ -16,14 +16,12 @@ int main(void)
 	std::cout << "5명의 나이 입력:";
 	for (i = 0; i < 5; i++)
 	{
-		
-		scanf_s("%d", &pi[i]);
+		scanf_s("%d", &pi[i]); //메모리를 배열처럼 사용하여 값 입력
 		sum += pi[i];
 	}
 
 	std::cout << "5명의 평균 나이:" << sum / 5.0 << std::endl;
 	free(pi);
-
 
 	return 0;
 }
