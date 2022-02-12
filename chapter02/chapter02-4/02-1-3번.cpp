2ff0767731c022118c734271bb5f73ba09aeb621
예린 정 <jung08140070@gmail.com>
2022-02-12 22:25:27 +0900

    220212

--- chapter02/chapter02-4/02-1-3번.cpp
+++ chapter02/chapter02-4/02-1-3번.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void SwapPointer(int &val1, int &val2)
+void SwapPointer(int *(&val1), int *(&val2))
 {
-	cout << val1 << ' ' << val2 << endl;
+	int *tmp;
+	tmp = val1;
+	val1 = val2;
+	val2 = tmp;
+
 }
 
 int main(void)
 {
+	
 	int num1 = 5;
 	int* ptr1 = &num1;
 	int num2 = 10;
 	int* ptr2 = &num2;
-
 	SwapPointer(ptr1, ptr2);
+	cout << *ptr1 << ' ' << *ptr2 << endl;
 	
 	return 0;
 }
